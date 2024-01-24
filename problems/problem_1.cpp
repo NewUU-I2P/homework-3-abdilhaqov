fddbcfa087d69e86278c6acb6bd4a2c0579874bc
abdilhaqov <147370775+abdilhaqov@users.noreply.github.com>
2024-01-24 17:06:34 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,6 +1,12 @@
 void problemSolution1(float consumed_water) {
     float cost;
     // write your code here
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
 
     return cost;
 }
